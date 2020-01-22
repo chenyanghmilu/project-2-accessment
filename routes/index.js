16708677cab2f125463813cedacacc88627dcb9d
Yang Chen <chenyanghmilu@gmail.com>
2020-01-22 11:29:40 -0500

    spaces&tabs

--- routes/index.js
+++ routes/index.js
@@ -4,20 +4,20 @@ const todos = require("../models/todo");
 
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Unit 2 Assessment', todos });
+    res.render('index', { title: 'Unit 2 Assessment', todos });
 });
 
 router.post("/", function(req, res) {
-  todos.push({
-    todo: req.body.todo,
-    done: false
-  });
-  res.redirect("/");
+    todos.push({
+        todo: req.body.todo,
+        done: false
+    });
+    res.redirect("/");
 });
 
 router.delete("/:index", function(req, res) {
-  todos.splice(req.params.index, 1);
-  res.redirect("/");
+    todos.splice(req.params.index, 1);
+    res.redirect("/");
 });
 
 module.exports = router;
